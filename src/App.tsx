726bee7451441814bccda606e5af36e9ef48c07b
Rafa Marquini <rafa.mardegan@gmail.com>
2022-02-21 15:08:13 -0300

    Add react-modal and polished dependencies; Add transaction modal component; Add styles to the transaction modal component;

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
+// Defines the application's main DOM element to disable users action
+// when the modal is opened. This enable accuracy on accessibility features.
+Modal.setAppElement('#root')
+
 export function App() {
+
+  // State and methods to control the NewTransactionModal
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
     </>
   );
 }
